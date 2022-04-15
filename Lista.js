9d3fbc2d4408d76c7dae307efd1e21603e380ebd
Andrlb2k1 <86635909+Andrlb2k1@users.noreply.github.com>
2022-04-15 13:13:40 -0300

    Create Lista.js

---
+++ Lista.js
@@ -0,0 +1,31 @@
+let input = document.getElementById("itemInput");
+let button = document.getElementById("itemBt");
+let ul = document.getElementById("itens");
+let itens = document.getElementsByTagName("li");
+let itemBts = document.getElementsByClassName("remove");
+
+function elmt(){
+  let li = document.createElement("li");
+  let XBt = document.createElement("button");
+  XBt.innerHTML = "X";
+  XBt.className = "remove";
+  li.appendChild(XBt);
+  let text = document.createTextNode(input.value);
+  li.appendChild(text);
+  ul.appendChild(li);
+  input.value = "";
+  btEvents()
+}
+
+function deleteIt(){
+  this.parentElement.remove();
+}
+
+function btEvents(){
+  for(let i = 0; i < itemBts.length; i++){
+    itemBts[i].addEventListener("click", deleteIt);
+  }
+}
+
+button.addEventListener("click", elmt);
+btEvents();
